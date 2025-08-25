7fecfe55ece56a73d6ee5734c9150e3d3ecd41f1
Crewlobbymadgaon <combinedcrewlobbymadgaon@gmail.com>
2025-08-25 18:58:15 +0530

    Update sw-user.js

--- sw-user.js
+++ sw-user.js
@@ -1,30 +1,13 @@
-const CACHE_NAME = "vc-user-cache-v2";
-const ASSETS = [
-  "/",              // root (loads index.html automatically)
-  "/favicon.png",
-  "/icon-192.png",
-  "/icon-512.png"
-];
-
-// Install: pre-cache app shell
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("vc-user-v1").then(cache => {
+      return cache.addAll(["./index.html", "./manifest.json", "./icon-192.png"]);
+    })
   );
 });
 
-// Activate: cleanup old caches
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
-    )
-  );
-});
-
-// Fetch: network falling back to cache
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
   );
 });
